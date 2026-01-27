a62c37a73aa66b3a8d833f0a3dfbcab7e38fa6ee
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-01-27 23:20:35 +0500

    seeder cleaned up

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,19 +9,10 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // High-speed bulk insertion (single query)
-//        User::factory()->count(100)->insert();
 
         $this->call(UsersSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
